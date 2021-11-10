1eb34f29b702bdf920176ccc52dd32da50f3d2f3
BerBie-s0 <welty.alex67@protonmail.ch>
2021-11-10 15:37:07 +0100

    test question 7

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ def createGrid(cols, rows):
     Returns:
         [array (2nd)]: [array of 2nd which have x cols and y rows]
     """
-    return [[ " " for i in range(cols)] for j in range(rows)]
+    return [[" " for i in range(cols)] for j in range(rows)]
 
 def randomCoord(grid):
     """[function randomCoord : function create an array(2nd). first degrees stands for the number of coord. second degree contains random pair of coord (x,y) array(1nd). 0<x<19, 0<y<49]
@@ -220,6 +220,39 @@ def addStepToGrid(grid, coord, path):
         cpt+=1
     return grid
 
+matrix=[
+[30.610455730027933, 19.235384061671343, 7.211102550927978, 17.08800749063506, 10.816653826391969, 18.110770276274835, 18.973665961010276, 6.4031242374328485],
+[19.235384061671343, 21.0, 15.297058540778355, 9.486832980505138, 13.45362404707371, 5.0990195135927845, 9.055385138137417, 17.0],
+[7.211102550927978, 15.297058540778355, 23.430749027719962, 16.97056274847714, 13.0, 16.1245154965971, 18.439088914585774, 11.0],
+[17.08800749063506, 9.486832980505138, 16.97056274847714, 30.14962686336267, 7.0, 4.47213595499958, 2.0, 12.041594578792296],
+[10.816653826391969, 13.45362404707371, 13.0, 7.0, 31.622776601683793, 9.848857801796104, 9.0, 5.0990195135927845],
+[18.110770276274835, 5.0990195135927845, 16.1245154965971, 4.47213595499958, 9.848857801796104, 26.019223662515376, 4.0, 14.317821063276353],
+[18.973665961010276, 9.055385138137417, 18.439088914585774, 2.0, 9.0, 4.0, 30.01666203960727, 14.035668847618199],
+[6.4031242374328485, 17.0, 11.0, 12.041594578792296, 5.0990195135927845, 14.317821063276353, 14.035668847618199, 32.64965543462902],
+]
+def nearest(matrix):
+    path=[]
+    min=matrix[0][0]
+    for a in range(len(matrix)):
+        if matrix[a][a]<min:
+            min=matrix[a][a]
+            pos=a+1
+    path.append(pos)
+
+    a=0
+    print(path)
+    while len(path)<8:
+        min=100
+        for b in range(len(matrix[path[a]-1])):
+            if (matrix[path[a]-1][b]<min) and (b+1 not in path) and (matrix[path[a]-1][b]!=matrix[b][b]):
+                min=matrix[path[a]-1][b]
+                pos=b+1
+        print(pos)
+        path.append(pos)
+        a+=1
+    return path
+print(nearest(matrix))
+
 def main():
     coord=randomCoord(createGrid(2,8))
     
@@ -236,11 +269,11 @@ def main():
 
     #showGrid(coord)
     #showGrid(grid)
-    #showGrid(matrix)
+    showGrid(matrix)
 
     grid=addStepToGrid(grid, coord, minPath)
     #showGrid(grid)
     #print(minPath)
     
 
-main()
+#main()
